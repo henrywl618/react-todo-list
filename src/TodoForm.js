28153b70dc43d1588ca9ed223a828b5629719d34
Henry <henrywlam+github@gmail.com>
2022-07-25 17:26:46 -0400

    completed todo-list mvp, added edit funtion, and mark completed function

---
+++ src/TodoForm.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react";
+
+const TodoForm = ({submitAction, removeTodo,title,edit=false,id})=>{
+
+    let [todoInput,setTodoInput] = useState("");
+
+    const handleChange = (e)=>{
+        setTodoInput(e.target.value);
+    };
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        edit ? submitAction(id,todoInput) : submitAction(todoInput);
+        setTodoInput("");
+    };
+
+    return (
+        <form>
+            <h3>{title}</h3>
+            <label htmlFor="todo"></label>
+            <input type="text"name="todo" id="todo" onChange={handleChange} value={todoInput}/>
+            <button onClick={handleSubmit}>Submit</button>
+        </form>
+    );
+};
+
+export default TodoForm;
